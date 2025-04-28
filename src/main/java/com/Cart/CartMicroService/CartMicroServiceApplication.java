07902acfb74f01ec9f3785c867f5194a6e07f6ea
ryszardpanda <rychu48@op.pl>
2025-04-28 19:03:36 +0200

    +initial commit with Spring

---
+++ src/main/java/com/Cart/CartMicroService/CartMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Cart.CartMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartMicroServiceApplication.class, args);
+	}
+
+}
