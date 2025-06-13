50fb90b365da87aed093f30bb33239651135d1e2
ryszardpanda <rychu48@op.pl>
2025-06-13 23:44:23 +0200

    +added swagger documentation for endpoints

--- src/main/java/com/Cart/CartMicroService/controller/CartController.java
+++ src/main/java/com/Cart/CartMicroService/controller/CartController.java
@@ -3,6 +3,11 @@ package com.Cart.CartMicroService.controller;
 import com.Cart.CartMicroService.mapper.CartMapper;
 import com.Cart.CartMicroService.model.dto.cart.CartResponseDTO;
 import com.Cart.CartMicroService.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +21,32 @@ public class CartController {
     private final CartService cartService;
     private final CartMapper cartMapper;
 
+    @Operation(summary = "Get cart By id", tags = "Cart")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cart successfully returned",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CartResponseDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CartResponseDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found",
+                    content = {@Content}),
+    })
     @GetMapping("/{id}")
     public CartResponseDTO getCartById(@PathVariable Long id){
         log.info("New request for endpoint GET/api/cart/{id} logged");
         return cartMapper.toDto(cartService.getCart(id));
     }
 
+    @Operation(summary = "Delete cart", tags = "Cart")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cart successfully deleted",
+                    content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Not found",
+                    content = {@Content}),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {@Content})
+    })
     @DeleteMapping("/{id}")
     public void deleteCart(@PathVariable Long id){
         log.info("New request for endpoint DELETE/api/cart/{id} logged");
--- src/main/java/com/Cart/CartMicroService/controller/CartItemController.java
+++ src/main/java/com/Cart/CartMicroService/controller/CartItemController.java
@@ -4,6 +4,11 @@ import com.Cart.CartMicroService.mapper.CartMapper;
 import com.Cart.CartMicroService.model.dto.cart.CartResponseDTO;
 import com.Cart.CartMicroService.model.dto.cartItem.CartItemRequestDTO;
 import com.Cart.CartMicroService.service.CartItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +22,32 @@ public class CartItemController {
     private final CartItemService cartItemService;
     private final CartMapper cartMapper;
 
+    @Operation(summary = "Add item to the cart", tags = "CartItem")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Item successfully added to cart",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CartResponseDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CartResponseDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found",
+                    content = {@Content}),
+    })
     @PostMapping("/{userId}/item")
     public CartResponseDTO addItem(@PathVariable String userId, @RequestBody CartItemRequestDTO cartItemRequestDTO) {
         log.info("New request for endpoint POST/api/cart/{userId}/item logged");
       return cartMapper.toDto(cartItemService.addItem(userId, cartItemRequestDTO));
     }
 
+    @Operation(summary = "Remove item from cart", tags = "CartItem")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Item successfully removed from cart",
+                    content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Not found",
+                    content = {@Content}),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {@Content})
+    })
     @DeleteMapping("/{cartId}/item/{cartItemId}")
     public void removeItem(@PathVariable Long cartId, @PathVariable Long cartItemId){
         log.info("New request for endpoint DELETE/api/cart/{userId}/item logged");
