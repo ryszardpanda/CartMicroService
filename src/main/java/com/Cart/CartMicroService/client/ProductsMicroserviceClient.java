ff9d78dbee84e0b1b508907fd7ffe5673c5f2f66
ryszardpanda <rychu48@op.pl>
2025-06-17 22:44:48 +0200

    +fixed micro-service name in connection string

--- src/main/java/com/Cart/CartMicroService/client/ProductsMicroserviceClient.java
+++ src/main/java/com/Cart/CartMicroService/client/ProductsMicroserviceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "products-microservice", url = "${base.url.products-micro-service}")
+@FeignClient(value = "products-microservice", url = "${base.url.products-microservice}")
 public interface ProductsMicroserviceClient {
 
     @GetMapping()
